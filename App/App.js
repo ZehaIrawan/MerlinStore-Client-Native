c33038552055c1d195762916964e961500071f25
Zeha Irawan <jangkaringbumi@gmail.com>
2020-02-11 16:13:14 +0700

    cleaning up boilerplate in App.js

---
+++ App/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Login />
+    </Fragment>
+  );
+};
+
+export default App;
